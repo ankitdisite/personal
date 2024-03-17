184a3b48938139dd679b8a3e87e01e98c6dbdf6d
ankitsulinder <ankit.sulinder@earlysalary.com>
2024-03-18 00:38:59 +0530

    dnt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: 'black', color: 'white', minHeight: '100vh', padding: '20px'}}>
+      <h1>Download Moon Image</h1>
+      <select onChange={handleDropdownChange} style={{marginBottom: '20px', fontSize: '16px', padding: '10px'}}>
+        <option value="144">144p</option>
+        <option value="440">440p</option>
+        <option value="720">720p</option>
+        <option value="1080">1080p</option>
+       
+        
+      </select>
+
+      <div>
+      {selectedOption && selectedOption==="1080" &&(
+          <img
+            src={require('./nikita.jpeg')}
+            //alt={selectedOption}
+            style={{ maxWidth: '600px', maxHeight: '500px' }}
+          />
+        )}
+        {selectedOption && selectedOption==="144" &&(
+          <img
+            src={require('./moon144.jpeg')}
+            //alt={selectedOption}
+            style={{ maxWidth: '1000px', maxHeight: '900px' }}
+          />
+        )}
+        {selectedOption && selectedOption==="440" &&(
+          <img
+            src={require('./moon440.jpeg')}
+            //alt={selectedOption}
+            style={{ maxWidth: '1000px', maxHeight: '900px' }}
+          />
+        )}
+        {selectedOption && selectedOption==="720" &&(
+          <img
+            src={require('./moon720.jpg')}
+            //alt={selectedOption}styl
+            style={{ maxWidth: '1000px', maxHeight: '1000px' }}
+          />
+        )}
+        { selectedOption==="" &&(
+          <img
+            src={require('./moon144.jpeg')}
+            //alt={selectedOption}styl
+            style={{ maxWidth: '1000px', maxHeight: '1000px' }}
+          />
+        )}
+        {/* {selectedOption &&  (
+          <img
+            src={require(selectedOption)}
+            alt={selectedOption}
+            style={{ maxWidth: '300px', maxHeight: '300px' }}
+          />
+        )} */}
+      </div>
     </div>
   );
 }
